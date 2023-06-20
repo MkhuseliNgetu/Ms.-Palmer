347b8ff99b245bb4f191186045daa72037f552ac
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2023-06-20 09:34:22 +0200

    User Interface #1.5 -Added loading screen. -Added a suggested config view.

--- Ms.Palmer/obj/Debug/Ms.Palmer_Content.g.i.cs
+++ Ms.Palmer/obj/Debug/Ms.Palmer_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("exit%20imagebutton.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("progressbar%20(regular%20size).gif")]
 
 
