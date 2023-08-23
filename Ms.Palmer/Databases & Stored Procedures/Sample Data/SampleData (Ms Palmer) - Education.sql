4c5d29af103e614da4fd65f00ed0cb902c8f7f24
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2023-08-23 13:33:11 +0100

    Machine Learning #3
    -Fixed some errors with sample data
    -Created a bacpac for ease of use when trying to access & read data in a SQL server.

--- Ms.Palmer/Databases & Stored Procedures/Sample Data/SampleData (Ms Palmer) - Education.sql
+++ Ms.Palmer/Databases & Stored Procedures/Sample Data/SampleData (Ms Palmer) - Education.sql
@@ -1,182 +1,362 @@
 USE VirtualMachineConfigurations; 
-
-//Sample Data
-//Educaton, 30GB
-EXEC @AddConfig 
+GO
+--Sample Data
+--Education, 30GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Windows 10',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Ubuntu',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Fedora',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Pop OS',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Linux Mint',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Educaton, 60GB
-EXEC @AddConfig 
+GO
+--Education, 30GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
+@UseCase = 'Entertainment',
+@OperatingSystem = 'Windows 10',
+@Size = 30720,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Ubuntu',
+@Size = 30720,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Fedora',
+@Size = 30720,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Pop OS',
+@Size = 30720,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Linux Mint',
+@Size = 30720,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+--Education, 60GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Windows 10',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Ubuntu',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Fedora',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Pop OS',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Linux Mint',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Educaton, 90GB
-EXEC @AddConfig 
+GO
+--Education, 60GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
+@UseCase = 'Education',
+@OperatingSystem = 'Windows 10',
+@Size = 61440,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
+@UseCase = 'Education',
+@OperatingSystem = 'Ubuntu',
+@Size = 61440,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Fedora',
+@Size = 61440,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Pop OS',
+@Size = 61440,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Linux Mint',
+@Size = 61440,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+--Education, 90GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Windows 10',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Ubuntu',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Fedora',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
-@UseCase = 'Education',
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Entertainment',
 @OperatingSystem = 'Pop OS',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Linux Mint',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Educaton, 120GB
-EXEC @AddConfig 
+GO
+--Education, 90GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Windows 10',
+@Size = 92160,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Ubuntu',
+@Size = 92160,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Fedora',
+@Size = 92160,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
+@UseCase = 'Education',
+@OperatingSystem = 'Pop OS',
+@Size = 92160,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Linux Mint',
+@Size = 92160,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+--Education, 120GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Windows 10',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Ubuntu',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Fedora',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Pop OS',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Linux Mint',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Educaton, 256GB
-EXEC @AddConfig 
+GO
+--Education, 120GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Windows 10',
+@Size = 122880,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Ubuntu',
+@Size = 122880,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Fedora',
+@Size = 122880,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Pop OS',
+@Size = 122880,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Linux Mint',
+@Size = 122880,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+--Education, 256GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Windows 10',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Ubuntu',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Fedora',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Pop OS',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Education',
 @OperatingSystem = 'Linux Mint',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
+GO
+--Education, 256GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Windows 10',
+@Size = 262144,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Ubuntu',
+@Size = 262144,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Fedora',
+@Size = 262144,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Pop OS',
+@Size = 262144,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
+@UseCase = 'Education',
+@OperatingSystem = 'Linux Mint',
+@Size = 262144,
+@OperatingSystemLicense = 'N/A',
+@GuestAdditions = 1;
--- Ms.Palmer/Databases & Stored Procedures/Sample Data/SampleData (Ms Palmer) - Work.sql
+++ Ms.Palmer/Databases & Stored Procedures/Sample Data/SampleData (Ms Palmer) - Work.sql
@@ -1,360 +1,360 @@
 USE VirtualMachineConfigurations; 
-
-//Sample Data
-//Work, 30GB, No-Guest-Additions.
-EXEC @AddConfig 
+GO
+--Sample Data
+--Work, 30GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Work, 30GB, Guest-Additions-Present.
-EXEC @AddConfig 
+GO
+--Work, 30GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 30720,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-//Work, 60GB, No-Guest-Additions.
-EXEC @AddConfig 
+GO
+--Work, 60GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Work, 60GB, Guest-Additions-Present.
-EXEC @AddConfig 
+GO
+--Work, 60GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 61440,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-//Work, 90GB, No-Guest-Additions.
-EXEC @AddConfig 
+GO
+--Work, 90GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Work, 90GB, Guest-Additions-Present.
-EXEC @AddConfig 
+GO
+--Work, 90GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 92160,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-//Work, 120GB, No-Guest-Additions.
-EXEC @AddConfig 
+GO
+--Work, 120GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Work, 120GB, Guest-Additions-Present.
-EXEC @AddConfig 
+GO
+--Work, 120GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 122880,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-//Work, 256GB, No-Guest-Additions.
-EXEC @AddConfig 
+GO
+--Work, 256GB, No-Guest-Additions.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig 
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 0;
-
-//Work, 256GB, Guest-Additions-Present.
-EXEC @AddConfig 
+GO
+--Work, 256GB, Guest-Additions-Present.
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Windows 10',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Ubuntu',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Fedora',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Pop OS',
 @Size = 262144,
 @OperatingSystemLicense = 'N/A',
 @GuestAdditions = 1;
-
-EXEC @AddConfig 
+GO
+EXEC VirtualMachineConfigurations.dbo.AddConfig
 @UseCase = 'Work',
 @OperatingSystem = 'Linux Mint',
 @Size = 262144,
